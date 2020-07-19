1595fcedf4e83cb59d5e7538e07ccc11134df3b6
jgray1 <john.edwin.gray@gmail.com>
2020-07-20 11:02:30 +1200

    get gameboard to display

--- minesweeper.js
+++ minesweeper.js
@@ -1,7 +1,18 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
 // Define your `board` object here!
-// var board = 
+var board = {
+  cells: [{row: 1, col: 1, isMine: '', hidden: ''},
+    {row: 1, col: 2, isMine: '', hidden: true},
+    {row: 1, col: 3, isMine: '', hidden: true},
+    {row: 2, col: 1, isMine: '', hidden: true},
+    {row: 2, col: 2, isMine: '', hidden: true},
+    {row: 2, col: 3, isMine: '', hidden: true},
+    {row: 3, col: 1, isMine: '', hidden: true},
+    {row: 3, col: 2, isMine: '', hidden: true},
+    {row: 3, col: 3, isMine: '', hidden: true}
+  ]
+}
 
 function startGame () {
   // Don't remove this function call: it makes the game work!
